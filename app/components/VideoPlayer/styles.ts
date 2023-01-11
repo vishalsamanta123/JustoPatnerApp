e235890c3650a12c487530a9578f0bb593017cdf
amanpatidar1 <amanpatidar@itinformatix.com>
2023-01-11 13:05:07 +0530

    changes on support forum

---
+++ app/components/VideoPlayer/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  videoStyle: {
+    height: "90%",
+    width: "95%",
+    alignSelf: "center",
+  },
+  playbtntch: {
+    position: "absolute",
+    justifyContent: "center",
+    alignItems: "center",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    top: "70%",
+  },
+});
+
+export default styles;
